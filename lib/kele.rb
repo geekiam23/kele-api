0917d65beaf810ee2136652acef3c919fb415820
Will Larry <will23larry@gmail.com>
2017-07-17 17:30:05 -0500

    Retrieved Mentor Availabilty

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,10 +4,10 @@ require "json"
   class Kele
     include HTTParty
     attr_reader :auth_token
-    base_uri 'https://www.bloc.io/api/v1'
+    base_uri "https://www.bloc.io/api/v1"
 
     def initialize(email, password)
-      response = self.class.post('/sessions', body: {"email": email, "password": password})
+      response = self.class.post("/sessions", body: {"email": email, "password": password})
       case response.code
         when 200
           puts "Great job, It is up and running!"
@@ -21,8 +21,14 @@ require "json"
     end
 
     def get_me
-      response = self.class.get('/users/me', headers: {"authorization" => @auth_token})
+      response = self.class.get("/users/me", headers: {"authorization" => @auth_token})
       @user_data = JSON.parse(response.body)
       @user_data
     end
+
+    def get_mentor_availability(mentor_id)
+      response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token})
+      JSON.parse(response.body)
+    end
   end
+# "mentor_id"=>2345139
